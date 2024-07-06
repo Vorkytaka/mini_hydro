a96514fc74fbb9d790645c18732d05ec03aee372
Vorkytaka <vorkytaka@gmail.com>
2024-07-07 00:51:30 +0300

    Base for app

--- miniHydro/ContentView.swift
+++ miniHydro/ContentView.swift
@@ -7,55 +7,118 @@
 
 import SwiftUI
 import SwiftData
+import HealthKit
+
+class Manager: ObservableObject {
+    @Published var permissionStatus: HKAuthorizationStatus = .notDetermined
+    
+    @AppStorage("storedNumber") private var storedNumber: Double?
+    @Published var inputValue: String = ""
+    
+    var isNumberStored: Bool {
+        return storedNumber != nil
+    }
+    
+    var storedNumberValue: Double? {
+        return storedNumber
+    }
+    
+    func saveNumber() {
+        if let value = Double(inputValue) {
+            storedNumber = value
+        }
+    }
+    
+    
+    private var healthStore = HKHealthStore()
+    
+    init() {
+        checkHealthKitPermission()
+    }
+    
+    func requestPermission() {
+        healthStore.requestAuthorization(toShare: [waterType], read: []) { success, error in
+            DispatchQueue.main.async {
+                self.permissionStatus = self.checkWaterPermission()
+            }
+        }
+    }
+    
+    func checkHealthKitPermission() {
+        self.permissionStatus = self.checkWaterPermission()
+    }
+    
+    private func checkWaterPermission() -> HKAuthorizationStatus {
+        return healthStore.authorizationStatus(for: waterType)
+    }
+    
+    private let waterType = HKObjectType.quantityType(forIdentifier: .dietaryWater)!
+    
+    func hydrate() {
+        let quantity = HKQuantity(unit: HKUnit.literUnit(with: .milli), doubleValue: storedNumberValue!)
+        let x = HKQuantitySample(type: waterType, quantity: quantity, start: .now, end: .now)
+        healthStore.save(x) { success, error in }
+    }
+}
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-
+    @StateObject var manager = Manager()
+    
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
+        VStack {
+            let status = manager.permissionStatus
+            if(status == .sharingDenied) {
+                Text("You reject permission")
+            } else if (status == .notDetermined) {
+                Button(action: {
+                    manager.requestPermission()
+                }) {
+                    Text("Request Permission")
                 }
-                .onDelete(perform: deleteItems)
+            } else if (manager.storedNumberValue == nil) {
+                TextField("Enter a number", text: $manager.inputValue)
+                    .keyboardType(.decimalPad)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                
+                Button(action: {
+                    manager.saveNumber()
+                }) {
+                    Text("Submit")
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+            } else {
+                Button(action: {
+                    manager.hydrate()
+                }) {
+                    Text("HYDRATE")
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
         }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
+        .onAppear {
+            manager.checkHealthKitPermission()
         }
     }
 }
 
+@main
+struct MyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+
+
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
 }
