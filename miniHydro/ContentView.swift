9dbe9afd720ded38ce5c629d6949dacdb14792ac
Vorkytaka <vorkytaka@gmail.com>
2024-07-07 02:33:51 +0300

    Separate UI and business logic

--- miniHydro/ContentView.swift
+++ miniHydro/ContentView.swift
@@ -9,60 +9,105 @@ import SwiftUI
 import SwiftData
 import HealthKit
 
-class Manager: ObservableObject {
-    @Published var permissionStatus: HKAuthorizationStatus = .notDetermined
+class Manager {
+    private static let volumeKey = "volume"
+    private static let volumeValueKey = "volumeValue"
+    private static let volumeUnitKey = "volumeUnit"
     
-    @AppStorage("storedNumber") private var storedNumber: Double?
-    @Published var inputValue: String = ""
+    static let shared = Manager()
     
-    var isNumberStored: Bool {
-        return storedNumber != nil
-    }
-    
-    var storedNumberValue: Double? {
-        return storedNumber
-    }
-    
-    func saveNumber() {
-        if let value = Double(inputValue) {
-            storedNumber = value
-        }
-    }
-    
-    
-    private var healthStore = HKHealthStore()
-    
-    init() {
-        checkHealthKitPermission()
-    }
-    
-    func requestPermission() {
-        healthStore.requestAuthorization(toShare: [waterType], read: []) { success, error in
-            DispatchQueue.main.async {
-                self.permissionStatus = self.checkWaterPermission()
-            }
-        }
-    }
-    
-    func checkHealthKitPermission() {
-        self.permissionStatus = self.checkWaterPermission()
-    }
-    
-    private func checkWaterPermission() -> HKAuthorizationStatus {
-        return healthStore.authorizationStatus(for: waterType)
-    }
+    private let healthKit = HKHealthStore()
+    private let userDefaults = UserDefaults()
     
     private let waterType = HKObjectType.quantityType(forIdentifier: .dietaryWater)!
     
+    private init() {}
+    
+    func saveVolume(quantity: Double, unit: HKUnit) {
+        let dictionaryToSave = [
+            Manager.volumeValueKey: quantity,
+            Manager.volumeUnitKey: unit.unitString
+        ] as [String : Any]
+        
+        userDefaults.set(dictionaryToSave, forKey: Manager.volumeKey)
+    }
+
+    func getVolume() -> HKQuantity? {
+        if let savedDictionary = userDefaults.dictionary(forKey: Manager.volumeKey),
+           let value = savedDictionary[Manager.volumeValueKey] as? Double,
+           let unitString = savedDictionary[Manager.volumeUnitKey] as? String {
+            let unit = HKUnit(from: unitString)
+            return HKQuantity(unit: unit, doubleValue: value)
+        }
+        return nil
+    }
+    
+    func requestHealthKitPermission(completion: @escaping (Bool, (any Error)?) -> Void) {
+        healthKit.requestAuthorization(toShare: [waterType], read: []) { success, error in
+            completion(success, error)
+        }
+    }
+    
+    func checkHealthKitPermission() -> HKAuthorizationStatus {
+        return healthKit.authorizationStatus(for: waterType)
+    }
+    
     func hydrate() {
-        let quantity = HKQuantity(unit: HKUnit.literUnit(with: .milli), doubleValue: storedNumberValue!)
-        let x = HKQuantitySample(type: waterType, quantity: quantity, start: .now, end: .now)
-        healthStore.save(x) { success, error in }
+        let quantity = getVolume()
+        if(quantity == nil) {
+            return
+        }
+        let sample = HKQuantitySample(type: waterType, quantity: quantity!, start: .now, end: .now)
+        healthKit.save(sample) { success, error in }
+    }
+}
+
+class UIManager: ObservableObject {
+    private let manager = Manager.shared
+
+    @Published var volume: HKQuantity? = nil
+    @Published var permissionStatus: HKAuthorizationStatus = .notDetermined
+    @Published var inputValue: String = ""
+    
+    init() {
+        checkHealthKitPermission()
+        checkVolume()
+    }
+    
+    func requestPermission() {
+        manager.requestHealthKitPermission(completion: { success, error in
+            DispatchQueue.main.async {
+                self.checkHealthKitPermission()
+            }
+        })
+    }
+    
+    func checkHealthKitPermission() {
+        self.permissionStatus = manager.checkHealthKitPermission()
+    }
+    
+    func checkVolume() {
+        volume = manager.getVolume()
+    }
+    
+    func hydrate() {
+        manager.hydrate()
+    }
+    
+    func setVolume() {
+        if let quantity = Double(inputValue) {
+            manager.saveVolume(quantity: quantity, unit: HKUnit.literUnit(with: .milli))
+            checkVolume()
+        }
+    }
+    
+    func hasVolume() -> Bool {
+        return manager.getVolume() != nil
     }
 }
 
 struct ContentView: View {
-    @StateObject var manager = Manager()
+    @StateObject var manager = UIManager()
     
     var body: some View {
         VStack {
@@ -75,14 +120,14 @@ struct ContentView: View {
                 }) {
                     Text("Request Permission")
                 }
-            } else if (manager.storedNumberValue == nil) {
+            } else if (!manager.hasVolume()) {
                 TextField("Enter a number", text: $manager.inputValue)
                     .keyboardType(.decimalPad)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .padding()
                 
                 Button(action: {
-                    manager.saveNumber()
+                    manager.setVolume()
                 }) {
                     Text("Submit")
                         .padding()
